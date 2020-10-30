c5f4492437a440687e198427522c412dfe776ced
Ruchir Dixit <ruchirtd96@gmail.com>
2020-10-30 20:13:48 +0530

    [Ruchir]Added: Main branch commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "ScalaPracticeProblems"
+
+version := "0.1"
+
+scalaVersion := "2.13.3"
---
+++ src/main/scala/com/bridgelabz/scalapracticeproblems/ReplaceString.scala
@@ -0,0 +1,10 @@
+package com.bridgelabz.scalapracticeproblems
+
+/**
+ * Created By Admin on Oct , 2020
+ */
+class ReplaceString {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
