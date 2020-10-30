bd5423b81c6de295ab353777241e030d2d45d628
Ruchir Dixit <ruchirtd96@gmail.com>
2020-10-30 23:29:42 +0530

    [Ruchir]Refactored: Code to use regular expressions and add comments

--- src/main/scala/com/bridgelabz/scalapracticeproblems/ReplaceString.scala
+++ src/main/scala/com/bridgelabz/scalapracticeproblems/ReplaceString.scala
@@ -1,12 +1,24 @@
 package com.bridgelabz.scalapracticeproblems
 
 /**
- * Created By Admin on Oct , 2020
+ * author - Admin
+ * date - 30/10/20
+ * time - 11:00 PM
+ * package - com.bridgelabz.scalapracticeproblems
+ * title - Program to Replace String
  */
+
+import scala.util.matching.Regex
 object ReplaceString {
+
+  // It accepts username from user and if it matches the expression then prints "Hello <<username>>, How are you?"
   def main(args: Array[String]): Unit = {
+    val pattern = new Regex("^[a-zA-Z]{3,}$")
       println("Enter username:")
       val userName = scala.io.StdIn.readLine()
+      if (pattern.matches(userName))
       println("Hello " + userName + ", How are you?")
+      else
+        println("Does not match!")
   }
 }
