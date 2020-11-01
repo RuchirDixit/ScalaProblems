2ac3d44339d0a769efc6c2ae63f534347282743e
Ruchir Dixit <ruchirtd96@gmail.com>
2020-11-01 13:51:13 +0530

    [Ruchir]Added: BC6 compute prime factors of number accepted from user

---
+++ src/main/scala/com/bridgelabz/scalapracticeproblems/PrimeFactors.scala
@@ -0,0 +1,56 @@
+package com.bridgelabz.scalapracticeproblems
+
+/**
+ * author - Ruchir
+ * date - 01-11-2020
+ * time - 13:17
+ * package - com.bridgelabz.scalapracticeproblems
+ * title - Print prime factors of number N
+ */
+import scala.util.control.Breaks._
+object PrimeFactors {
+
+  val Two = 2
+  // accepts number and prints its prime factors
+  def printPrimeFactors() = {
+    try {
+      var isPrime = false
+      println("Enter number to find factors")
+      var range = Utilities.getIntegerInput()
+      while (range % Two == 0) {
+        println(Two + " ");
+        range /= Two;
+      }
+      var counter = 3
+      while ( {
+        counter <= Math.sqrt(range)
+      }) { // While counter divides range, print counter and divide range
+        while ( {
+          range % counter == 0
+        }) {
+          println(counter + " ")
+          range /= counter
+        }
+
+        counter += Two
+      }
+      if (range > Two)
+        println(range);
+    }
+    catch
+      {
+        case _ : ArithmeticException =>
+        {
+          println("Error, Cannot divide by zero")
+        }
+        case _ : NumberFormatException =>
+        {
+          println("Error, Expected value is Integer")
+        }
+      }
+  }
+  // main method calls printPrimeFactors method
+  def main(args: Array[String]): Unit = {
+    printPrimeFactors()
+  }
+}
